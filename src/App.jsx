b7b11b25d56c924a0b4dc8eaa6f5adc306a42270
7PiSeth <7piseth@gmail.com>
2025-08-23 18:06:24 +0700

    update routing position always on the top when open new page

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,29 @@ import NavBar from "./components/NavBar";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Footer from "./components/Footer";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { routes } from "./components/pages/data/router"; // import from data.js
+
+// ScrollToTop component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" }); // scroll smoothly to top
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => {
   useEffect(() => {
     AOS.init({ duration: 1000, offset: 200, anchorPlacement: "top-bottom" });
     AOS.refresh();
   }, []);
+
   return (
     <Router>
+      <ScrollToTop /> {/* 👈 Add this */}
       <div className="max-w-screen-lg mx-auto min-h-screen flex flex-col justify-between items-center">
         <NavBar />
         <Routes>
@@ -25,4 +38,5 @@ const App = () => {
     </Router>
   );
 };
+
 export default App;
